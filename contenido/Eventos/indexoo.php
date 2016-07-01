c6b6e935f8eaa2b01af97bc7aa5d2fead80181de
kevinarrietah <kevinarrietah@hotmail.com>
2016-07-01 15:40:07 -0500

    subida de imagen y carpeta evento
    todos los eventos adversos

---
+++ contenido/Eventos/indexoo.php
@@ -0,0 +1,174 @@
+<html>
+<head>
+<title>Formulario de eventos adversos</title>
+<meta charset="utf-8">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <meta charset="utf-8">
+</head>
+<BODY BGCOLOR="#D8D8D8"> 
+<br>
+<div id="estilo1">
+<h2 >Bienvenido a su sistema de ingreso de eventos adversos en Epsil&oacute;n</h2>
+</div>
+   <br>
+
+    <form class="form-horizontal" role="form"id="formulario" action="enviado.php" method="post">
+    <fieldset>
+        <legend><FONT FACE="arial" SIZE=4 ;>Registro de detallende eventos adversos</FONT></legend>
+<style>
+      table {
+    border-collapse: separate;
+  border-spacing:  8px;
+  
+}
+td, th {
+      color: #000;
+}
+
+p {
+  font-family: cursive;
+
+  font-size: 100%;
+color: 
+}
+
+legend {
+    display: block;
+    padding-left: 4px;
+    padding-right: 4px;
+    border: none;
+    color: #0088B2
+}
+div.centraTabla{
+text-align: center;
+color: #0088B2
+
+}
+
+div.centraTabla table {
+margin: 60px;
+text-align: left;
+color: #0088B2
+}
+
+</style>
+<div class='centraTabla'>
+<table border="0">
+<form >
+    <tr>
+    <div class="form-group">
+        <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+        </tr>
+        <div class="col-xs-4">
+           <tr><td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero">
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+           </td></tr>
+
+        </div>
+    </div>
+<tr>
+    <div>
+        <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+        </tr>
+        <div class="col-xs-4">
+           <tr><td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero">
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+           </td></tr>
+
+        </div>
+    </div>
+    <tr>
+    <div >
+        <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+        </tr>
+        <div class="col-xs-4">
+           <tr><td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero">
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+           </td></tr>
+
+        </div>
+    </div>
+    <tr>
+    <div >
+        <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+        </tr>
+        <div class="col-xs-4">
+           <tr><td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero">
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+           </td></tr>
+
+        </div>
+    </div>
+    <tr>
+    <div>
+       <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+        </tr>
+        <div class="col-xs-4">
+           <tr><td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero">
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+           </td></tr>
+
+        </div>
+    </div>
+
+
+
+
+    <tr>
+
+<br>
+<tr>
+    <div>
+        <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+         <td><p>Numero total de empleados:</p></td>
+        </tr>
+        <div class="col-xs-4">
+           <tr><td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero">
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+<td> <input type="email" class="form-control" id="inputEmail" placeholder="Numero"></td>
+           </td></tr>
+
+        </div>
+    </div></td><tr>
+
+    
+</form></table>
+</div>
+<br>
+    <div class="form-group">
+        <div class="col-xs-offset-4 col-xs-9">
+            <input type="submit" class="btn btn-primary" value="Enviar">
+            <input type="reset" class="btn btn-default" value="Limpiar">
+        </div>
+  
+
+</body>
+</html>
